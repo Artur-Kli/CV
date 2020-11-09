f3bd4c0a2e9a2752ec5f1cdd4c040404a050818e
Artur-Kli <arturklinfo@gmail.com>
2020-11-10 02:49:20 +0300

    Добавил ссылки Data

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -5,7 +5,7 @@ export const portfolioData = [
     languages: ['Diferent','LP'],
     languagesIcons: ['./media/icons/react.svg', './media/icons/firebase.svg', './media/icons/styled_component.svg'],
     project: 'https://ultra-lp.web.app/',
-    source: 'https://github.com/Artur-Kli',
+    source: 'https://github.com/Artur-Kli/Ultra-LP',
     info: 'Responsive React Website using Styled Components. I used React Hooks and React Router. The site is made with reusable components that I can customize and place wherever you want.',
     picture: './media/ultra-lp.png'
   },
@@ -15,7 +15,7 @@ export const portfolioData = [
     languages: ['Diferent','Ecommerce'],
     languagesIcons: ['./media/icons/react.svg', './media/icons/firebase.svg', './media/icons/material_ui.png', './media/icons/calendar.png'],
     project: 'https://airbnb-clone-73df7.web.app/',
-    source: 'https://github.com/Artur-Kli',
+    source: 'https://github.com/Artur-Kli/Airbnb-clone',
     info: 'This React project has multiple pages: Home, Search. In this React app, I use: React router, useState, useHistory, DateRangePicker and Material-ui.',
     picture: './media/airbnb.png'
   },
@@ -25,7 +25,7 @@ export const portfolioData = [
     languages: ['Diferent','Entertainments'],
     languagesIcons: ['./media/icons/react.svg', './media/icons/firebase.svg', './media/icons/styled_component.svg', './media/icons/fuse_js.png', './media/icons/testing_library.png'],
     project: 'http://netflix-abe80.web.app',
-    source: 'https://github.com/Artur-Kli',
+    source: 'https://github.com/Artur-Kli/Netflix-clone',
     info: 'This React project has multiple pages: Home, Browse (which uses Firebase authentication), sign in, and sign up. Login connects to Firebase when the user tries to sign in, and when the user signs up, we use Firebase auth to store the user in the Firebase auth database. In this React app, I use Styled Components for all the styling I implement. I use React knowledge(useEffect, useState, useContext) and JavaScript knowledge for using higher order functions such as map, filter, reduce, and find. I also use React router for navigation, Fuse.js for live search, and testing all components using React Testing Library.',
     picture: './media/netflix.png'
   },
@@ -35,7 +35,7 @@ export const portfolioData = [
     languages: ['Diferent','Entertainments'],
     languagesIcons: ['./media/icons/react.svg', './media/icons/firebase.svg', './media/icons/material_ui.png'],
     project: 'https://clone-3ab38.web.app/',
-    source: 'https://github.com/Artur-Kli',
+    source: 'https://github.com/Artur-Kli/Youtube-clone',
     info: 'This React project has multiple pages: Home, Search. I used: React router, useState and Material-ui.',
     picture: './media/youtube.png'
   },
@@ -45,7 +45,7 @@ export const portfolioData = [
     languages: ['Diferent','CRM'],
     languagesIcons: ['./media/icons/vue.svg', './media/icons/firebase.svg', './media/icons/materialize.png'],
     project: 'https://accountant-crm-442f4.web.app',
-    source: 'https://github.com/Artur-Kli',
+    source: 'https://github.com/Artur-Kli/Accountant-CRM',
     info: 'Accountant CRM have 2 locale ru-en. Данный VUE проект имеет 9 страниц (с разным layout), подключенных через lazy loading. Также реализована динамическая валидация с библиотекой Vuelidate и шина для ошибок с Vuex. Создана защита роутов, универсальная пагинация с помощью mixin, на основе существующих данных создан график с vue-chartjs и создан фильтр динамической локализации (RU-EN). ',
     picture: './media/accountant-crm.png'
   },
@@ -55,7 +55,7 @@ export const portfolioData = [
     languages: ['Diferent','Ecommerce'],
     languagesIcons: ['./media/icons/react.svg', './media/icons/firebase.svg', './media/icons/redux.svg', './media/icons/lodash.svg', './media/icons/semantic-ui.svg'],
     project: 'https://bookstore-4dfd9.web.app',
-    source: 'https://github.com/Artur-Kli',
+    source: 'https://github.com/Artur-Kli/Bookstore',
     info: 'Данный React проект имеет поиск по книгам и добавление товара в корзину. Используемые технологии: Redux, Lodash, Semantic UI.',
     picture: './media/bookstore.png'
   },
